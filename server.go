720573f7a284af94b24ee0fa16088ef4fe752bb1
vl <vl@playtini.ua>
2020-01-10 17:30:58 +0200

    flag "api-version"

--- server.go
+++ server.go
@@ -11,9 +11,12 @@ import (
 	"strconv"
 )
 
+var dockerApiVersion string
+
 func main() {
 	var port int
 	flag.IntVar(&port, "port", 8080, "Port")
+	flag.StringVar(&dockerApiVersion, "api-version", "v1.40", "Docker API version")
 	flag.Parse()
 
 	http.HandleFunc("/healthcheck", healthcheck)
@@ -33,7 +36,7 @@ func serverNo(w http.ResponseWriter, r *http.Request) {
 		"-s",
 		"--unix-socket",
 		"/var/run/docker.sock",
-		"http:/v1.40/containers/"+path+"/json")
+		"http:/"+dockerApiVersion+"/containers/"+path+"/json")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
